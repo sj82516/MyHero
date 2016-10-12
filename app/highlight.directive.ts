7d33263568695674050850ed9e46eba3cca8ce17
zhengyuanjie <sj82516@gmail.com>
2016-10-13 07:07:29 +0800

    chap2-1-plus host event

--- app/highlight.directive.ts
+++ app/highlight.directive.ts
@@ -6,10 +6,22 @@ import {Directive, ElementRef, Renderer, Input, OnInit, HostListener} from '@ang
 export class HighlightDirective implements OnInit{
     private defaultBorderColor:string = 'red';
     @Input() borderColor:string='';
+    @HostListener('mouseenter',['$event']) onMouseEnter;
+    @HostListener('mouseleave',['$event']) onMouseLeave;
 
     constructor(private el:ElementRef, private renderer: Renderer) {
     }
     ngOnInit(){
         this.renderer.setElementStyle(this.el.nativeElement, 'border', '4px solid '+ (this.borderColor || this.defaultBorderColor));
+    }
+
+    onMouseEnter(ev){
+        console.log(ev);
+        this.renderer.setElementStyle(this.el.nativeElement, 'border', '10px solid');
+    }
+
+    onMouseLeave(ev){
+        console.log(ev);
+        this.renderer.setElementStyle(this.el.nativeElement, 'border-width', '4px');
     }
 }
